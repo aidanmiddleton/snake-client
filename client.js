c7970ddf485d7c61f87cb446d2e27982f154ce30
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-12 21:26:50 +0000

    added ability to send name to server

--- client.js
+++ client.js
@@ -7,6 +7,10 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log('Connection made, allez-la');
+    conn.write("Name: aem");
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   }); 
